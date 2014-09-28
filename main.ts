28783eb573dca115f1102a4bd69ba05a266bfc99
Sander Mak <sander.mak@luminis.eu>
2014-09-28 14:12:21 -0700

    Show and check input

--- main.ts
+++ main.ts
@@ -3,12 +3,29 @@
 
 module Main {
 
-    console.log(new Tower.Tower());
-    console.log("Starting")
+    class TowerController {
+        base: number = 8
+        max: number = 10
+        tower: Tower.Tower
+        attempt: number[][]
+        result: Tower.Result
+
+        constructor() {
+           this.refresh()
+        }
+
+        check():void {
+            this.result = this.tower.check(this.attempt)
+        }
+
+        refresh():void {
+            this.tower = new Tower.Tower(this.base, this.max);
+            this.attempt = this.tower.getStartingPoint()
+            this.result = undefined
+        }
+    }
+
     var towerModule = angular.module('tower', [])
     towerModule.controller('TowerController', TowerController)
 
-    class TowerController {
-        msg = "foo"
-    }
 }
